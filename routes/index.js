c16e2947d85bfbdb090c69ac8cd1194a5f747f3d
Aleksandar <aleksandar.m.trifunovic@gmail.com>
2021-02-11 22:59:29 +0100

    created schemas,models,hbs pages and some routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,46 @@
-const router = require("express").Router();
+const router = require("express").Router(); //explanation for this line of code if possible pls.
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  res.render("homepage.hbs");
 });
+
+router.get("/guestSignUp", (req, res, next) => {
+  res.render("guest-signUp.hbs");
+});
+
+router.get("/ownerSignUp", (req, res, next) => {
+  res.render("owner-signUp.hbs");
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("login.hbs");
+});
+
+router.get("/login/searchPage", (req, res, next) => {
+  res.render("search.hbs");
+});
+
+router.get("/searchPage/overviewGuest", (req, res, next) => {    //ask the explanation of creating static vs dinamic routes
+  res.render("overview-guest.hbs");
+});
+
+// router.get("/", (req, res, next) => {
+//   res.render("index");
+// });
+
+// router.get("/", (req, res, next) => {
+//   res.render("index");
+// });
+
+// router.get("/", (req, res, next) => {
+//   res.render("index");
+// });
+
+// router.get("/", (req, res, next) => {
+//   res.render("index");
+// });
+
+
 
 module.exports = router;
