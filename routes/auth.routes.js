2438f57842c914b46b6441f0f62dfb4be612e0c9
Aleksandar <aleksandar.m.trifunovic@gmail.com>
2021-02-14 14:18:54 +0100

    two pages created, search all owners and search all guests

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-// const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 
 //importing guest an owner models
 const guestModel = require("../models/guest.model.js");
@@ -88,22 +88,34 @@ router.get("/logIn", (req, res, next) => {
 
 router.post("/logIn", (req, res, next) => {
   const { logInName, logInEmail, guestPassword } = req.body;
-  console.log(req.body)
-    if (!logInName.length || !logInEmail.length || !guestPassword.length) {
-      res.render("authorisation/login.hbs", { msg: "Please enter all fields" });
-      return;
-    }
-     let re = /\S+@\S+\.\S+/;
-     if (!re.test(logInEmail)) {
-       res.render("auth/signup", { msg: "Email not in valid format" });
-       return;
-     }
+  res.redirect("/");
+  console.log(req.body);
+  // if (!logInName.length || !logInEmail.length) {
+  //   res.render("authorisation/login.hbs", { msg: "Please enter all fields" });
+  //   return;
+  // }
+  //email validation
+  // let re = /\S+@\S+\.\S+/;
+  // if (!re.test(logInEmail)) {
+  //   res.render("auth/signup", { msg: "Email not in valid format" });
+  //   return;
+  // }
+  // PASWORD VALIDATION
+  // let regexPass = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{8,20}$/;
+  // if (!regexPass.test(password)) {
+  //    res.render('auth/signup', {msg: 'Password needs to have special characters, some numbers and be 6 characters atleast'})
+  //    return;
+  // }
+  //   let salt = bcrypt.genSaltSync(10);
+  //   let hash = bcrypt.hashSync(password, salt);
+  //   UserModel.create({ username, email, password: hash });
 });
+
 
 //we are getting an error with the password when we try to login. 
 //if you are trying to log in from a guest acc then in if condition this needs to be present "!guestPassword.length".
 // if you are logging in from an owners acc then !ownerPassword.length should be there. find a way to make it dinamic perhaps. or whatever.
-
+// we have 2 models, one owner and one guest instead of one. How do we switch between them.
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -2,32 +2,22 @@
 
 const express = require("express")
 const router = express.Router()
+const guestModel = require("../models/guest.model.js");
+const ownerModel = require("../models/owners.models.js");
+
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("homepage.hbs");
 });
 
 
-
-router.get("/searchPage/overviewGuest", (req, res, next) => {    //ask the explanation of creating static vs dinamic routes
-  res.render("overview-guest.hbs");
+router.get("/allpetowners", (req,res, next)=> {
+  res.render("all-pet-owners.hbs");
 });
 
-// router.get("/", (req, res, next) => {
-//   res.render("index");
-// });
-
-// router.get("/", (req, res, next) => {
-//   res.render("index");
-// });
-
-// router.get("/", (req, res, next) => {
-//   res.render("index");
-// });
-
-// router.get("/", (req, res, next) => {
-//   res.render("index");
-// });
+router.get("/allpetlovers", (req, res, next) => {
+  res.render("all-pet-lovers.hbs");
+});
 
 
 
